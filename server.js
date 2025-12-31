797d2cf9f07cdf63d5ad4dc077cbefe4c237da9d
Harry Abong Kamilo <you@example.com>
2025-12-31 12:03:27 +1100

    Fix PORT for Render

--- server.js
+++ server.js
@@ -497,5 +497,7 @@ app.get("/requests", (req, res) => {
   `));
 });
 
-const PORT = 3000;
-app.listen(PORT, () => console.log("Server running on port", PORT));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => console.log("Server running on port", 
+PORT));
+
